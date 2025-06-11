7c2a96ccd2d77b875706b9411dd9a00b89244262
maxas10 <maxaseung@gmail.com>
2025-06-10 23:46:41 -0700

    display req

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -26,16 +26,5 @@
 
 export default function handler(req, res) {
   const verify_token = "birthday123";
-  const mode = req.query["hub.mode"];
-  const token = req.query["hub.verify_token"];
-  const challenge = req.query["hub.challenge"];
-
-  if (mode === "subscribe" && token === verify_token) {
-    console.log("✅ WEBHOOK VERIFIED");
-    res.status(200).send(challenge);
-  } else {
-    res.sendStatus(403);
-  }
-  console.log("running");
-  res.sendStatus(200);
+  res.status(200).send(req);
 }
